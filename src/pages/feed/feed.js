d41fd15fd64a27fae938fd541b0a179a7fc279e0
Gabriel Araujo <gabrielaraujo16@yahoo.com>
2023-05-07 09:47:06 -0300

    página inicial/login
    página de login na home criada, foi removido icones padrões do react que não serão utilizados

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -2,21 +2,8 @@ import './feed.css';
 
 function Feed() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+
     </div>
   );
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,14 +2,31 @@ import './home.css';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div className="Home">
+      <div>
+      <header className='header-home'>
+        <h1>Alavanque seu negócio conosco!</h1>
         <p>
-          Esta é a página Home.
+          Nossa plataforma vai ajudar sua empresa se desenvolver e crescer. <br/>
+          Tenha mentoria com os melhores do mercado. <br/>
+          Faça networking com empresas e mentores. <br/>
+          Faça parcerias comerciais com empreendedores da área do seu interesse!
         </p>
-
       </header>
+      { /* Container do formulário */ }
+      <div className="formulario">
+        <form>
+          <h2>Faça Login ou Cadastre-se</h2>
+          <label for="email">Email</label>
+          <input id= "email" type="text" placeholder="Email" />
+          <label for="senha">Senha</label>
+          <input id="senha" type="password" placeholder="Senha" />
+          <button className='button1' type="submit">Entrar</button>
+          <button type="submit">Cadastre-se</button>
+        </form>
+      </div>
+    </div>
+      
     </div>
   );
 }
