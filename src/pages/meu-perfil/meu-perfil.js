5ee2ce7c8e57acd840fce9411a461f9725d38bee
Caique Oliveira <Caiqueoliver15@yahoo.com>
2023-05-29 19:06:52 -0300

    Centralização dos textos e divs da landing page

--- src/pages/meu-perfil/meu-perfil.js
+++ src/pages/meu-perfil/meu-perfil.js
@@ -1,51 +1,20 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import Perfil from '../../components/perfil/perfil';
+
 import './meu-perfil.css';
-import Conexoes from '../../components/conexoes/conexoes';
-import Sobrenos from '../../components/sobrenos/sobrenos';
-import Postagens from '../../components/postagens/postagens';
-import img from "../../img/ficante-da-barbie.jpg";
-import image from "../../img/homemdeferro.jpg";
-import imagem from "../../img/cachorro.jpg";
 
 function MeuPerfil() {
   return (
-    <div className="background py-3">
-    <header className="background-header"></header>
-    
-      <Container>
-        <Row> 
+    <div className='mPerfil'>
+      <div className='Banner'>
 
-          <Col xs={12} sm={12} md={12} lg={4} xl={4} className='py-3'>
-           <Row>
-            <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-              <Perfil></Perfil>
-            </Col>
-            <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-              <Conexoes></Conexoes>
-            </Col>
-           </Row>
-            
-          </Col>
+      </div>
+      <div className='BackgroundFundo'>
+        <div className='FPerfil'>
 
-          <Col xs={12} sm={12} md={12} lg={5} xl={4} className='py-3'>
-            <Row>
-              <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                <Sobrenos></Sobrenos>
-              </Col>
+        </div>
+        <div className='ConteudoPerfil'>
 
-              <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                <Postagens></Postagens>
-              </Col>
-            </Row>
-          </Col>
-          
-          <Col xs={12} sm={12} md={12} lg={3} xl={4} className='py-3'>
-            Terceira coluna
-          </Col>
-
-        </Row>
-      </Container>
+        </div>
+      </div>
     </div>
   );
 }
