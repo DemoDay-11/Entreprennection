e72ef1f78b9aaf408f539032e1e1d82d380a9ae8
Luis Henrique <duimbispo@gmail.com>
2023-06-02 18:38:10 -0300

    Fiz o cadastramento

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 //Routes.js
 import { Route, Routes as RoutesDom } from "react-router-dom"
 import Login from "./pages/login/login";
+import Cadastramento from "./pages/cadastramento/cadastramento";
 
 import Menu from "./components/menu/menu";
 import Landing from "./pages/landing-page/landing";
@@ -10,6 +11,7 @@ const Routes = () => {
       <RoutesDom>
         <Route index element={<Landing />} />
         <Route path="Login" element={<Login />} />
+        <Route path="Signup" element={<Cadastramento />} />
         <Route path="Home" element={<Menu />} />
       </RoutesDom>
   )
