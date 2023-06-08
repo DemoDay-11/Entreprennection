e8fae85d42c6da5b0ad1736c26ec57724843febf
Luis Henrique <duimbispo@gmail.com>
2023-06-07 21:33:32 -0300

    Ajuste de layout da tela home.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,113 +1,73 @@
 import './home.css';
 import avatarRedondo  from '../../img/avatar-redondo.png';
-import fotoPublicacao from '../../img/conexões.jpeg';
-import { AiOutlineLike } from 'react-icons/ai';
-import { BiCommentAdd } from 'react-icons/bi';
-import { BiShare  } from 'react-icons/bi';
-import { BiMailSend  } from 'react-icons/bi';
+import { Col,Row } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Post from '../../components/post/post';
 import { FiSend  } from 'react-icons/fi';
-import { MdOutlineAddReaction  } from 'react-icons/md'
 
 
 
 function Home() {
   return (
-    <div className="Home">
-      <div className='Feed'>
-        <div className='FacaPub'>
-          <div className='ConteudoPerfil'>
-            <div className='FotoPerfil'>
-              <img src={avatarRedondo} width="80px" height="80px" className='img'></img>
-            </div>
-            <div className='TextoPub'>
-          
-            </div>
-          </div>
-        </div>
-        
-        <div className='Publicacao'>
-          <div className='parteCima'>
-            <div className='fotoPerfil2'>
-              <img src={avatarRedondo} width="60px" height="60px" className='img'></img>
-            </div>
-            <div className='nomesPerfil'>
-              <div className='nomePerfil'>
-                <p>Caique de Oliveira</p>
-              </div>
-              <div className='areaAPerfil'>
-                <p>Mentor</p>
-              </div>
-            </div>
-          </div>
-            <div className='TituloPubli'>
-              <p>Encontro realizado no dia 01/05/2023. Apenas amigos próximos. Foi um ótimo passeio e fico feliz de reencontrar vocês</p>
-            </div>
-            <div className='ConteudoFoto'>
-              <img src={fotoPublicacao} width='100%' height='100%'></img>
-            </div>
-            <div className='BtnsBaixo'>
-              <div className='Gostei'>
-                <button>
-                  <div className='iconeLike'><AiOutlineLike />Gostei</div>
-                </button>
-              </div>
-              <div className='Comentar'>
-                <button>
-                  <div className='iconeComentar'><BiCommentAdd />Comentar</div>
-                </button>
-              </div>
-              <div className='Compartilhar'>
-                <button>
-                  <div className='iconeCompartilhar'><BiShare />Compartilhar</div>
-                  </button>
-              </div>
-              <div className='Enviar'>
-                <button>
-                  <div className='iconeEnviar'><BiMailSend />Enviar</div>
-                  </button>
-              </div>
-            </div>
-            <div className='ComentariosPublicacao'>
-              <div className='CaixaFeedback'>
-                <div className='fotoPerfilComentario'>
-                  <img src={avatarRedondo} width="40px" height="40px" className='img'></img>
-                </div>
-                <div className='adicioneComentario'>
-                <label for="name"></label>
-                  <input type="text" id="name" name="name" required
-                  minlength="4" maxlength="30" size="30" placeholder='Digite o que achou sobre isso..'></input>
-                  <div className='enviarComentario'>
-                    <FiSend />
-                  </div>
-                  <div className='addReacao'>
-                    <MdOutlineAddReaction />
-                  </div>
-                </div>
-              </div>
-            </div>
-        </div>
+    <div className='Home'>
+      <Row className='m-0 p-0'>
+        <Col xs={12} sm={12} md={12} lg={10} xl={10} className='p-0'>
+          <div className='Feed pt-0 pb-3 mx-0'> 
+            <Row className='d-flex m-0'>
+              {/* container de nova publicação  */}
+              <Col xs={12} sm={12} md={12} lg={12} xl={12} className='d-flex justify-content-center p-0'>
+                <Row className='FacaPub d-flex align-items-center py-4'>
+                  <Col xs={2} sm={2} md={1} lg={1} xl={1}>
+                    <img src={avatarRedondo} width="48px" height="48px"></img>
+                  </Col>
+                  <Col xs={8} sm={8} md={10} lg={10} xl={10}>
+                    <Form.Control aria-label="First name" />
+                  </Col>
+                  <Col xs={2} sm={2} md={1} lg={1} xl={1} className='p-0'>
+                    <button className='send'>
+                      <FiSend />
+                    </button>    
+                  </Col>
+                </Row>
+              </Col>
 
-      </div>
+              {/* container de postagens */}
+              <Col xs={12} sm={12} md={12} lg={12} xl={12} className='p-0 d-flex justify-content-center' style={{height: "fit-content"}}>
+                <div className='post-home'>
+                  <Post/>
+                  <Post/>
+                  <Post/>
+                  <Post/>
+                  <Post/>
+                  <Post/>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+
+        <Col lg={2} xl={2} className='p-0 d-none d-sm-none d-md-none d-lg-block' style={{backgroundColor: "#023859"}}>
+          <div className='Trending'>
+            <div className='Noticias'>
+              <p>Notícias do momento</p>
+            </div>
+            <div className='CardsNoticiais'>
+              <div className='Noticia1'>
+                <p>Texto da Noticia1</p>
+              </div>
+              <div className='Noticia2'>
+                <p>Texto da Noticia2</p>
+              </div>
+              <div className='Noticia3'>
+                <p>Texto da Noticia3</p>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+
       
-      <div className='Trending'>
-        <div className='Noticias'>
-          <p>Notícias do momento</p>
-        </div>
-        <div className='CardsNoticiais'>
-          <div className='Noticia1'>
-            <p>Texto da Noticia1</p>
-          </div>
-          <div className='Noticia2'>
-          <p>Texto da Noticia2</p>
-          </div>
-          <div className='Noticia3'>
-          <p>Texto da Noticia3</p>
-          </div>
-          <div className='Noticia4'>
-          <p>Texto da Noticia4</p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
