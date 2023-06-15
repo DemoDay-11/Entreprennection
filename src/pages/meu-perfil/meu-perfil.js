7c5c2be82fc439480501e913afd3ea27b3f54c05
Caique Oliveira <Caiqueoliver15@yahoo.com>
2023-06-15 00:42:27 -0300

    Começo do meu Perfil

--- src/pages/meu-perfil/meu-perfil.js
+++ src/pages/meu-perfil/meu-perfil.js
@@ -1,18 +1,24 @@
 import './meu-perfil.css';
+import FotoPerfil from './components/FotoPerfil';
 
 function MeuPerfil() {
   return (
     <div className='meuPerfil'>
+        <div className='FotoPerfil'>
+        </div>
         <div className='Banner'>
 
         </div>
-        <div className='FotoPerfil'>
-
-        </div>
+        
       <div className='BackgroundFundo'>
         
         <div className='ConteudoPerfil'>
-
+          <div className='NomePerfil'>
+            <p>Bubu Modas</p>
+          </div>
+          <div className='AreaAtuacao'>
+            
+          </div>
         </div>
       </div>
     </div>
